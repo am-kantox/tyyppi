4b84c451e5a00ac5aa257de5a0b791d6949e3848
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-01-02 08:22:54 +0100

    charlists: :as_lists

--- .iex.exs
+++ .iex.exs
@@ -4,7 +4,7 @@ if File.exists?(global_settings), do: Code.require_file(global_settings)
 Application.put_env(:elixir, :ansi_enabled, true)
 
 IEx.configure(
-  inspect: [limit: :infinity],
+  inspect: [limit: :infinity, charlists: :as_lists],
   colors: [
     eval_result: [:cyan, :bright],
     eval_error: [[:red, :bright, "\n▶▶▶\n"]],
