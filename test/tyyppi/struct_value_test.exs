2874fa9f8dd0731f7bba540c1b657b938a051a45
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-01-09 17:46:42 +0100

    More tests

--- test/tyyppi/struct_value_test.exs
+++ test/tyyppi/struct_value_test.exs
@@ -1,13 +1,17 @@
 defmodule Test.Tyyppi.StructValue do
   use ExUnit.Case
 
-  alias Tyyppi.{ExampleValue, Value}
+  alias Tyyppi.{ExampleValue, Struct, Value}
 
   test "struct with all values" do
     ex = %ExampleValue{}
 
     assert %ExampleValue{bar: %Value{value: 5}} = put_in(ex, [:bar], 5)
     assert %ExampleValue{foo: %Value{value: :atom}} = put_in(ex, [:foo], 'atom')
+    assert %ExampleValue{foo: %Value{value: :atom}} = Struct.update!(ex, :foo, fn _ -> 'atom' end)
+
+    assert {:error, [coercion: [message: "Expected atom(), charlist() or binary()", got: 5]]} =
+             Struct.update(ex, :foo, fn _ -> 5 end)
 
     assert_raise BadStructError, "expected a struct named Tyyppi.ExampleValue, got: :baz", fn ->
       put_in(ex, [:baz], 5)
