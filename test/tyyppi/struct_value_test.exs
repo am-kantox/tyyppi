8d4e42947ccdfc0639769ddab16ea3b6d69cf3de
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-01-09 17:44:23 +0100

    Value is implemented for atom and integer

--- test/tyyppi/struct_value_test.exs
+++ test/tyyppi/struct_value_test.exs
@@ -7,9 +7,12 @@ defmodule Test.Tyyppi.StructValue do
     ex = %ExampleValue{}
 
     assert %ExampleValue{bar: %Value{value: 5}} = put_in(ex, [:bar], 5)
+    assert %ExampleValue{foo: %Value{value: :atom}} = put_in(ex, [:foo], 'atom')
 
-    # assert %ExampleValue{bar: %Value{__meta__: %{errors: [_ | _]}, value: 42}} =
-    #          put_in(ex, [:bar], :ok)
+    assert_raise BadStructError, "expected a struct named Tyyppi.ExampleValue, got: :baz", fn ->
+      put_in(ex, [:baz], 5)
+    end
+
     assert_raise ArgumentError, ~r"\Acould not put/update key :bar with value :ok", fn ->
       put_in(ex, [:bar], :ok)
     end
